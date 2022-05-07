f2447c3700af398a737a610eb8d4099614c2c8b3
Alen-43 <98441795+Alen-43@users.noreply.github.com>
2022-05-07 16:17:21 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+from pyrogram import Client
+
+
+PyrogramBot = Client(
+    "PyrogramBot",
+    bot_token="5102623936:AAGA7i5H0M80ZM6jqXiHWo4nu4fIA474fJA",
+    api_id="10429496",
+    api_hash="dc86c8623259c10c4fd1d1b7d8dab96f",
+    plugins=dict(root="")
+)
+
+
+
+PyrogramBot.run()
