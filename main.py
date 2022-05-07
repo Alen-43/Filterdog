ae1c5566e5295c1cfb04e12b040b3f28727e8e2e
Alen-43 <98441795+Alen-43@users.noreply.github.com>
2022-05-07 16:18:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ PyrogramBot = Client(
     bot_token="5102623936:AAGA7i5H0M80ZM6jqXiHWo4nu4fIA474fJA",
     api_id="10429496",
     api_hash="dc86c8623259c10c4fd1d1b7d8dab96f",
-    plugins=dict(root="")
+    plugins=dict(root="bot")
 )
 
 
